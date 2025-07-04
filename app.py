7e3025bc1c2eca258bd155fc510c2f644db41e27
obongg <esthermonday3@gmail.com>
2025-07-04 15:53:44 +0100

    updated html

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "I am almost a devops engineer — This was created by Obongg"
+    return render_template("index.html", year=datetime.now().year)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
